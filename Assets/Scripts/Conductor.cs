c5d00749a77ab0df2bbe931b6231b731cfdda162
overcast-day <58866988+overcast-day@users.noreply.github.com>
2023-07-18 10:54:08 -0700

    added audiotrack and test delay music and soundeffects

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class Conductor : MonoBehaviour
 {
     [Header("Audio Source")]
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource sfxSource;
+    [SerializeField] AudioSource MusicSource;
+    [SerializeField] AudioSource SfxSource;
 
     [Header("Audio Source")]
-    public AudioClip countdown;
+    public AudioClip Countdown;
+    public AudioClip SongMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        sfxSource.clip = countdown;
-        sfxSource.Play();
-        //Debug.Log("Countdown Started");
+        SfxSource.clip = Countdown;
+        SfxSource.PlayScheduled(AudioSettings.dspTime);
+        Debug.Log("Countdown Started" + " at dspTime " + AudioSettings.dspTime);
+
+        MusicSource.clip = SongMusic;
+        MusicSource.PlayScheduled(AudioSettings.dspTime + 5);
+        Debug.Log("Song will start 5 seconds" + " from dspTime " + AudioSettings.dspTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
