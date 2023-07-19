c1a49b1f109226bcb579f4ca30615e23a8114748
Jessica Qiu <74211080+jeqiu@users.noreply.github.com>
2023-07-19 09:22:28 -0700

    basic working version

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
+using UnityEngine.Networking;
+using System;
 
 public class Conductor : MonoBehaviour
 {
@@ -11,12 +16,93 @@ public class Conductor : MonoBehaviour
     [Header("Audio Source")]
     public AudioClip countdown;
 
+    public static Conductor Instance;
+
+    public Lane[] lanes;
+    public float songDelayInSeconds;
+    public double marginOfError; // in seconds
+
+    public int inputDelayInMilliseconds;
+
+    public string fileLocation;
+    public float noteTime; // time note will be on screen
+    public float noteSpawnY;
+    public float noteTapY;
+    public float noteDespawnY
+    {
+        get
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+
+    public static MidiFile midiFile;
+
     // Start is called before the first frame update
     void Start()
     {
-        sfxSource.clip = countdown;
-        sfxSource.Play();
+        Instance = this;
+
+        if (Application.streamingAssetsPath.StartsWith("http://") || Application.streamingAssetsPath.StartsWith("https://"))
+        {
+            StartCoroutine(ReadFromWebsite());
+        }
+        else
+        {
+            ReadFromFile();
+        }
+    }
+
+    private IEnumerator ReadFromWebsite()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + fileLocation))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                byte[] results = www.downloadHandler.data;
+                using (var stream = new MemoryStream(results))
+                {
+                    midiFile = MidiFile.Read(stream);
+                    GetDataFromMidi();
+                }
+            }
+        }
+    }
+
+    private void ReadFromFile()
+    {
+        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+        GetDataFromMidi();
+    }
+
+    public void GetDataFromMidi()
+    {
+        var notes = midiFile.GetNotes();
+        var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+        notes.CopyTo(array, 0);
+
+        foreach (var lane in lanes) lane.SetTimeStamps(array);
+
+        Invoke(nameof(StartSong), songDelayInSeconds);
+    }
+
+    public void StartSong()
+    {
+        // sfxSource.clip = countdown;
+        // sfxSource.Play();
         //Debug.Log("Countdown Started");
+        musicSource.Play();
+    }
+
+    public static double GetAudioSourceTime()
+    {
+        return (double)Instance.musicSource.timeSamples / Instance.musicSource.clip.frequency;
     }
 
     // Update is called once per frame
