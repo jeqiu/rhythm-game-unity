2b5933c5974ac450a99e269e10fe82c6ce2be64f
Jessica Qiu <74211080+jeqiu@users.noreply.github.com>
2023-07-26 09:19:51 -0700

    add blinking return text in scoreboard scene

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 public class ScoreBoard : MonoBehaviour
 {
     public static ScoreBoard Instance;
-    public TMPro.TextMeshPro scoreText;
     public int gameScore;
+    public TMPro.TextMeshPro scoreText;
+    public TMPro.TextMeshPro returnText;
+    public float animSpeedInSec = 1f;
+    bool keepAnimating = false;
     private string MenuName = "Menu";
 
     // Start is called before the first frame update
@@ -17,6 +20,7 @@ public class ScoreBoard : MonoBehaviour
         gameScore = SharedData.score;
         scoreText.text = gameScore.ToString();
         SharedData.score = 0;
+        startReturnTextAnimation();
     }
 
     // Update is called once per frame
@@ -33,4 +37,79 @@ public class ScoreBoard : MonoBehaviour
         SceneManager.LoadScene(MenuName);
     }
 
+    private IEnumerator AnimateReturnText()
+    {
+        Color currentColor = returnText.color;
+        Color invisibleColor = returnText.color;
+        invisibleColor.a = 0; //Set Alpha to 0
+
+        float oldAnimSpeedInSec = animSpeedInSec;
+        float counter = 0;
+        while (keepAnimating)
+        {
+            //Hide Slowly
+            while (counter < oldAnimSpeedInSec)
+            {
+                if (!keepAnimating)
+                {
+                    yield break;
+                }
+
+                //Reset counter when Speed is changed
+                if (oldAnimSpeedInSec != animSpeedInSec)
+                {
+                    counter = 0;
+                    oldAnimSpeedInSec = animSpeedInSec;
+                }
+
+                counter += Time.deltaTime;
+                returnText.color = Color.Lerp(currentColor, invisibleColor, counter / oldAnimSpeedInSec);
+                yield return null;
+            }
+
+            yield return null;
+
+
+            //Show Slowly
+            while (counter > 0)
+            {
+                if (!keepAnimating)
+                {
+                    yield break;
+                }
+
+                //Reset counter when Speed is changed
+                if (oldAnimSpeedInSec != animSpeedInSec)
+                {
+                    counter = 0;
+                    oldAnimSpeedInSec = animSpeedInSec;
+                }
+
+                counter -= Time.deltaTime;
+                returnText.color = Color.Lerp(currentColor, invisibleColor, counter / oldAnimSpeedInSec);
+                yield return null;
+            }
+        }
+    }
+
+    // call to start animation
+    void startReturnTextAnimation()
+    {
+        if (keepAnimating)
+        {
+            return;
+        }
+        keepAnimating = true;
+        StartCoroutine(AnimateReturnText());
+    }
+
+    void changeReturnTextAnimationSpeed(float animSpeed)
+    {
+        animSpeedInSec = animSpeed;
+    }
+
+    void stopReturnTextAnimation()
+    {
+        keepAnimating = false;
+    }
 }
