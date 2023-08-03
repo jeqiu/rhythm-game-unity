348fa57348d39742b836ce306f6601ff26ee4604
Jessica Qiu <74211080+jeqiu@users.noreply.github.com>
2023-08-02 18:33:34 -0700

    update song track button

--- Assets/Scripts/SongTrackButton.cs
+++ Assets/Scripts/SongTrackButton.cs
@@ -5,22 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class TrackButtonUI : MonoBehaviour
 {
+    [SerializeField] private string GameInfo = "GameInfo";
     [SerializeField] private string TrackName1 = "Main";
     [SerializeField] private string TrackName2 = "Parody";
     [SerializeField] private string TrackName3 = "MainHSR";
 
     public void NewGame1()
     {
-        SceneManager.LoadScene(TrackName1);
+        SharedData.trackName = TrackName1;
+        SceneManager.LoadScene(GameInfo);
     }
 
     public void NewGame2()
     {
-        SceneManager.LoadScene(TrackName2);
+        SharedData.trackName = TrackName2;
+        SceneManager.LoadScene(GameInfo);
     }
 
     public void NewGame3()
     {
-        SceneManager.LoadScene(TrackName3);
+        SharedData.trackName = TrackName3;
+        SceneManager.LoadScene(GameInfo);
     }
+
 }
+
